d217c9a99c2c008826fe2d66ec3e5adcd35033f5
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2025-01-09 19:07:35 -0800

    updated config css and js files to make tailwind work as expected.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import IncomeStatement from "./IncomeStatement";
 import React from "react";
+import './global.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container)
