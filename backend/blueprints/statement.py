c9da71c64d29076096c3aef7a34bf193076638c8
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2025-01-11 00:51:07 -0800

    added balance sheet into router.

--- backend/blueprints/statement.py
+++ backend/blueprints/statement.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from filters.income_filter import IncomeFilter
+from handlers.balance_sheet_handler import BalanceSheetHandler
 from handlers.income_handler import IncomeHandler
 from settings import DATABASE_NAME, HOST
 
@@ -15,31 +15,22 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 income_handler = IncomeHandler(session)
+balance_sheet_handler = BalanceSheetHandler(session)
 
 
 @statement_bp.route("/income-statement", methods=["GET"])
 def get_income_statement():
     symbol = request.args.get("symbol")         # company symbol
+    page = int(request.args.get("page", 1))
+    
+    records = income_handler.read(symbol=symbol, page=page)
+    return jsonify(records)
 
-    filters = {
-        "date_range": {
-            "start": request.args.get("start_date"),
-            "end": request.args.get("end_date")
-        },
-        "revenue_range": {
-            "min": request.args.get("min_revenue", 0),
-            "max": request.args.get("max_revenue", float("inf"))
-        },
-        "net_income_range": {
-            "min": request.args.get("min_net_income", 0),
-            "max": request.args.get("max_net_income", float("inf"))
-        }
-    }
 
-    # should be reading from db, fetch should be separate
-    income_filter = IncomeFilter(session)
-    records = income_filter.get_filtered_records(symbol=symbol, filters=filters)
-
-    records = [record.to_dict() for record in records]
-
-    return jsonify({"data": records})
+@statement_bp.route("/balance-sheet-statement", methods=["GET"])
+def get_balance_sheet_statement():
+    symbol = request.args.get("symbol")
+    page = int(request.args.get("page", 1))
+    
+    records = balance_sheet_handler.read(symbol=symbol, page=page)
+    return jsonify(records)
