48db0cd254e0c3eb05dd09ba5798bc1c8791f7d5
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2025-01-12 02:55:52 -0800

    reformatted the code and switched off the debug mode.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+
 from blueprints.statement import statement_bp
 
 app = Flask(__name__)
@@ -7,10 +8,11 @@ app.register_blueprint(statement_bp, url_prefix="/api")
 
 CORS(app)
 
+
 @app.route("/")
 def index():
     return "ok"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
