125e91526cbc8139266df520f0bed84f0d627d2f
YJ-Junsheng-Ye <overdosedizzy@gmail.com>
2025-01-12 18:39:04 -0800

    restructured the components.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import IncomeStatement from "./IncomeStatement";
+import IncomeStatement from "./components/IncomeStatement";
 import React from "react";
 import './global.css';
-import BalanceSheetStatement from './BalanceSheetStatement';
-import CashFlowStatement from './CashFlowStatement';
+import BalanceSheetStatement from './components/BalanceSheetStatement';
+import CashFlowStatement from './components/CashFlowStatement';
 
 const container = document.getElementById("root");
 const root = createRoot(container)
